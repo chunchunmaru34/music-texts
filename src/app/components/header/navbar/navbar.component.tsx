217ae2f863463c239fb27106b4241d6f869a19a1
Aliaksei Labkovich <aliaksei.labkovich@itechart-group.com>
2019-08-28 17:09:48 +0300

    add icons to navbar

--- src/app/components/header/navbar/navbar.component.tsx
+++ src/app/components/header/navbar/navbar.component.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { withRouter, Link, RouteComponentProps } from 'react-router-dom';
+import { FiHome, FiMusic } from 'react-icons/fi';
 
 import * as styles from './navbar.styles.scss';
 
@@ -7,8 +8,14 @@ import * as styles from './navbar.styles.scss';
 export const NavbarComponent = withRouter(({ history, location }: RouteComponentProps) => {
   return (
     <div className={styles['navbar']}>
-      <Link className={styles['link']} to="/">Home</Link>
-      <Link className={styles['link']} to="/favourite-tracks">Your Tracks</Link>
+      <Link className={styles['link']} to="/">
+        <FiHome/>
+        <span className={styles['link-label']}>Home</span>
+      </Link>
+      <Link className={styles['link']} to="/favourite-tracks">
+        <FiMusic/>
+        <span className={styles['link-label']}>Your Tracks</span>
+      </Link>
     </div>
   )
 })
