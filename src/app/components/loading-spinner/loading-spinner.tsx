e9da388107844cf2963ab94ef8fddd97ecd51521
Aliaksei Labkovich <aliaksei.labkovich@itechart-group.com>
2019-09-24 17:18:04 +0300

    Remove spinners library

--- src/app/components/loading-spinner/loading-spinner.tsx
+++ src/app/components/loading-spinner/loading-spinner.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
-import { ClipLoader } from 'react-spinners';
 
-import * as styles from './loading-spinner.scss';
+import * as styleSheet from './loading-spinner.scss';
 
+type LoadingSpinnerProps = {
+  size?: number;
+  sizeUnit?: string;
+  color?: string;
+}
 
-export const LoadingSpinner = () => (
-  <div className={styles['sweet-loading']}>
-    <ClipLoader
-      sizeUnit={"px"}
-      size={75}
-      color={'#b7bccb'}
-    />
-  </div>
-)
+export const LoadingSpinner = ({ size = 75, sizeUnit = 'px', color = '#b7bccb' }: LoadingSpinnerProps) => {
+  const styles = {
+    width: `${size}${sizeUnit}`,
+    height: `${size}${sizeUnit}`,
+    borderColor: color,
+  }
+
+  return (
+    <div className={styleSheet['loading-spinner-container']}>
+      <div className={styleSheet['loading-spinner']} style={styles}/>
+    </div>
+  )
+}
